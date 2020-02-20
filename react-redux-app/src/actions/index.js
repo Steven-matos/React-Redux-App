e9804af40daa5952fe86325b3d7c58f779582ff4
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-02-19 19:30:35 -0500

    update

--- react-redux-app/src/actions/index.js
+++ react-redux-app/src/actions/index.js
@@ -5,7 +5,7 @@ export const UPDATE_ANIMES = "UPDATE_ANIMES";
 export const SET_ERROR = "SET_ERROR";
 
 export const getData = () => dispatch =>{
-    dispatch({ type: FETCH_DATA });
+    dispatch({ type: FETCH_DATA }); 
     axios.get('https://api.jikan.moe/v3/top/anime/1')
     .then(res => {
         console.log(res.data.top);
